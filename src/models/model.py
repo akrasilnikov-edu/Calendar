e200458376ae376f97407485fff3092d4989bac4
Andrey Krasilnikov <akrasilnikov@gmail.com>
2026-06-16 12:52:03 +0300

    feat: add class Emotion as frozen object, add  AbstractDay class ... see abstractmethods in child class

--- src/models/model.py
+++ src/models/model.py
@@ -1,21 +1,40 @@
-import calendar
+
+from dataclasses import dataclass
 import datetime
 from functools import total_ordering
+from abc import ABC, abstractmethod
+
+
+class AbstractDay(ABC):
+    @abstractmethod
+    def get_date(self)->datetime.date:...
+
+    @abstractmethod
+    def get_description(self)->str:...
+    
+    @abstractmethod
+    def set_emotion(self, emotion:Emotion)->None:...
+@dataclass(frozen=True)
+class Emotion:
+    img:str|None
+    emoji:str
+
+
+
 
 @total_ordering 
-class DayStruct(object):
+class DayStruct(AbstractDay):
     def __init__(self) -> None:
         self.date:datetime.date|None = None
-        self.emotion = None
-        self.description = None
+        self.emotion:Emotion|None = None
+        self.description:str = ""
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, DayStruct):
             return NotImplemented
         if self.date is None or other.date is None:
-            return self.date is other.date  # Both None or one None
-        return int(datetime.datetime.combine(self.date, datetime.time.min).timestamp()) == \
-               int(datetime.datetime.combine(other.date, datetime.time.min).timestamp())
+            return self.date is other.date
+        return self.date == other.date
     
     def __lt__(self, other):
         if not isinstance(other, DayStruct):
